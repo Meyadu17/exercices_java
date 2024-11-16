738a028cdd46812d69c146a49b663a769cc92121
Meya <angie-island@hotmail.fr>
2024-11-16 15:30:33 +0100

    First commit

---
+++ src/fr/apside/exerciceJava/exercice1/Lasagna.java
@@ -0,0 +1,32 @@
+package fr.apside.exerciceJava.exercice1;
+
+public class Lasagna {
+
+	private int tempsDeCuisson =40;
+
+	public int expectedMinutesInOven() {
+		System.out.println("Le temps de cuisson des lasagnes est de " + tempsDeCuisson + " minutes.");
+		return tempsDeCuisson;
+	}
+
+	public int remainingMinutesInOven(int minutesRestantes) {
+		this.tempsDeCuisson -= minutesRestantes;
+		System.out.println("Il reste " + this.tempsDeCuisson + " minutes de cuisson pour les lasagnes.");
+		return this.tempsDeCuisson;
+
+	}
+
+	public int preparationTimeInMinutes(int nombreDecouches) {
+		int tempsPasse =  nombreDecouches * 2;
+		System.out.println("Vous avez passé " + tempsPasse + " minutes de préparation des lasagnes.");
+		return tempsPasse;
+	}
+
+	public int totalTimeInMinutes(int nombreDecouches, int tempsDeCuissonFait) {
+		preparationTimeInMinutes(nombreDecouches);
+		int tempsDeTravail = tempsDeCuissonFait + preparationTimeInMinutes(nombreDecouches);
+		System.out.println("Le temps de travail total est de " + tempsDeTravail + " minutes.");
+		return tempsDeTravail;
+
+	}
+}
---
+++ src/fr/apside/exerciceJava/exercice4/exercice4.java
@@ -0,0 +1,24 @@
+package fr.apside.exerciceJava.exercice4;
+
+/**
+ * Exercice 4 - Héritage et Interface
+ * Temps passé : 60 minutes
+ * Problèmes rencontré :
+ */
+public class exercice4 {
+	public static void main(String[] args) {
+		/**
+		 * Dans cet exercice, vous jouez à un jeu de rôle nommé "Wizards and Warriors", qui vous permet d'incarner un sorcier ou un guerrier.
+		 * Il existe différentes règles pour les guerriers et les sorciers afin de déterminer le nombre de points de dégâts qu'ils infligent.
+		 * Pour un Warrior, voici les règles :
+		 *     - Infligez 6 points de dégâts si le combattant attaqué n'est pas vulnérable
+		 *     - Infligez 10 points de dégâts si le combattant attaqué est vulnérable
+		 * Pour un Wizard, voici les règles :
+		 *     - Infligez 12 points de dégâts si le Wizard a préparé un sort à l'avance
+		 *     - Infligez 3 points de dégâts si le Wizard n'a pas préparé de sort à l'avance
+		 * En général, les combattants ne sont jamais vulnérables. Cependant, les sorciers sont vulnérables s'ils n'ont pas préparé de sort.
+		 * Vous avez six tâches qui fonctionnent avec les guerriers et les sorciers (et bonus une septième avec les voleurs).
+		 */
+		//questio 1
+	}
+}
